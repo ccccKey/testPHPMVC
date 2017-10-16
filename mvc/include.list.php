1e68d7c5ef260e66843215d4c98770fbd39b06f1
ccccKey <670897678@qq.com>
2017-10-16 19:50:30 +0800

    2017-10-16

---
+++ mvc/include.list.php
@@ -0,0 +1,9 @@
+<?php
+   $path = array(
+      'function/function.php',
+      'libs/core/DB.class.php',
+      'libs/core/VIEW.class.php',
+      'libs/db/mysql.class.php',
+      'libs/view/Smarty.class.php'
+   	);
+?>
